a224fd06ea4284a89bf67a4bb636b70151598eaa
Ripon <habibkazi92@gmail.com>
2021-03-11 15:51:13 +0800

    all done

--- src/Component/TeamDetails/TeamDetails.js
+++ src/Component/TeamDetails/TeamDetails.js
@@ -3,6 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFlagCheckered, faFutbol, faMapMarker, faMarsStroke } from '@fortawesome/free-solid-svg-icons'
 import Male from '../../Images/male.png';
 import './TeamDetails.css';
+import twitter from '../../Images/twitter.png';
+import youtube from '../../Images/youtube.png';
+import facebook from '../../Images/facebook.png';
+import instagram from '../../Images/instagram.png';
+import website from '../../Images/website.png';
 const TeamDetails = (props) => {
     console.log(props.team);
     const {
@@ -21,16 +26,16 @@ const TeamDetails = (props) => {
                 <div className="col-sm-6 left-side">
                     <h3> {strAlternate}</h3>
                     <p> <FontAwesomeIcon icon={faMapMarker} /> &nbsp; Founded: {intFormedYear}</p>
-
                     <p> <FontAwesomeIcon icon={faFlagCheckered} /> &nbsp; Country : {strCountry}</p>
                     <p><FontAwesomeIcon icon={faFutbol} /> &nbsp;  Sport Type: {strLeague}</p>
                     <p> <FontAwesomeIcon icon={faMarsStroke} /> &nbsp; Gender: {strGender}</p>
+                    <a href={`https://${strWebsite}`} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faFutbol} /> &nbsp; {strWebsite} </a>
                 </div>
                 <div className="col-sm-6 right-side">
                     <img src={Male} alt="team-badge" />
                 </div>
             </div>
-            <div className="content-part ">
+            <div className="content-part">
                 <h4>Team Details:</h4>
                 <p>  
                     {strDescriptionEN}
@@ -40,20 +45,12 @@ const TeamDetails = (props) => {
                     {strStadiumDescription}
                 </p>
             </div>
-            <div>
-                
-            </div>
-            <p> {strAlternate}</p>
-            <p> {strTwitter}</p>
-            <p> {strWebsite}</p>
-            <p> {strYoutube}</p>
-            <p> {strStadium}</p>
-            <p> {strFacebook}</p>
-            <p> {strInstagram}</p>
-
-
-            <p> {intFormedYear}</p>
-
+            <div className="footer-item">
+              <a href={`https://${strTwitter}`} rel="noreferrer" target="_blank"> <img src={twitter} alt="stadium" className="header-img" /></a>
+              <a href={`https://${strYoutube}`} rel="noreferrer" target="_blank"> <img src={youtube} alt="stadium" className="header-img" /></a>
+              <a href={`https://${strFacebook}`} rel="noreferrer" target="_blank"> <img src={facebook} alt="stadium" className="header-img" /></a>
+              <a href={`https://${strInstagram}`} rel="noreferrer" target="_blank"> <img src={instagram} alt="stadium" className="header-img" /></a>
+            </div>      
         </div>
     );
 };
